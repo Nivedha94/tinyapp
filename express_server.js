e804ae5a7c814a23007a9d5de3a7049088f05cd3
vagrant <nivedha.selvan@gmail.com>
2021-11-11 07:01:43 +0000

    new route using route parameter

--- express_server.js
+++ express_server.js
@@ -25,7 +25,12 @@ app.get("/hello", (req, res) => {
 app.get("/urls", (req, res) => {
   templateVars = {urls: urlDatabase};
   res.render("urls_index", templateVars);
-})
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  templateVars = {shortURL: req.params.shortURL, longURL: req.params.longURL};
+  res.render("urls_show", templateVars);
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
